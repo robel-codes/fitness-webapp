4ea36d45fee8fbdf28e2d065f8d43e34a61fd08f
Robel Gebremeskel <rofikre@yahoo.com>
2025-05-20 00:29:40 -0400

    Added Contact Us section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses";
+import ContactUs from "@/scenes/contactUs";
 import { SelectedPage } from "@/shared/types";
 
 
@@ -41,6 +42,9 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <OurClasses
+        setSelectedPage={setSelectedPage}
+      />
+      <ContactUs 
         setSelectedPage={setSelectedPage}
       />
     </div>
